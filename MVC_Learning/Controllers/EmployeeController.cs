203c0468722d07285ebb362b7c5277a334976f32
Praveen Prajapati <itspraveenkp@gmail.com>
2020-12-28 19:20:44 +0530

    Basic MVC Learning

---
+++ MVC_Learning/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Learning.Models;
+
+namespace MVC_Learning.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        #region CommentedCode
+
+        //public string EmployeeProfile(int id)
+        //{
+        //    string profile = string.Empty;
+        //    if (id == 1)
+        //    {
+        //        profile = "Employee 1 Profile";
+        //    }
+        //    else if (id == 2)
+        //    {
+        //        profile = "Employee 1 Profile";
+        //    }
+        //    else
+        //    {
+        //        profile = "No Record found";
+        //    }
+
+        //    return profile;
+        //}
+
+        ////int? department = null;
+        //public string Address(int id, int? code=null)
+        //{
+        //    return "id = " + id + " Dept = " + code;
+        //}
+        #endregion
+
+        public ActionResult Index()
+        {
+            var data = getEmployee();
+            return View(data);
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        private Employee getEmployee()
+        {
+            return new Employee()
+            {
+                ID = 1,
+                Name = "Praveen",
+                Address = "Bandra"
+            };
+        }
+    }
+}
---
+++ MVC_Learning/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Learning.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region CommentedCode
+
+        //public string Index()
+        //{
+        //    return "Hello from index method Controller";
+        //}
+
+        //public string Name()
+        //{
+        //    return "Hello from Name method";
+        //}
+        #endregion
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Index()
+        {
+            return View("~/Views/MyView/NewView.cshtml");
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
